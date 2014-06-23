470c12a9f257ade8f3bff957632eba5067dee8a7
TheMasterRage <themasterragehd@gmail.com>
2014-06-23 14:16:27 +0100

    Advanced Maths
    It calculates Pythagoras.

---
+++ AdvancedMaths.py
@@ -0,0 +1,11 @@
+import math
+
+a = float(input("Enter the first line "))
+
+b = float(input("Enter the second line "))
+
+c_squared = a**2 + b**2
+
+c = c_squared**0.5
+
+print("The long line is ", c)
